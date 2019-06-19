6cf1d6d1d037cb31b2d42b47438eae5a8d3dd6f6
Patryk Woziński <patryk.wozinski@gmail.com>
2019-06-19 14:13:58 +0200

    turn off / start

---
+++ src/EngineModule/Application/Command/TurnOff/TurnOffCommand.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace App\EngineModule\Application\Command\TurnOff;
+
+
+final class TurnOffCommand
+{
+}
---
+++ src/EngineModule/Application/Command/TurnOff/TurnOffHandler.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\EngineModule\Application\Command\TurnOff;
+
+
+final class TurnOffHandler
+{
+	public function __invoke(TurnOffCommand $command): void
+	{
+	}
+}
