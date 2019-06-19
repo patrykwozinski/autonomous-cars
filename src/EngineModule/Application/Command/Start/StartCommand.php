e76dbb2f237674413a390ba2d950b71935ed6a8e
Patryk Woziński <patryk.wozinski@gmail.com>
2019-06-19 14:13:13 +0200

    application for engine

---
+++ src/EngineModule/Application/Command/Start/StartCommand.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace App\EngineModule\Application\Command\Start;
+
+
+final class StartCommand
+{
+}
---
+++ src/EngineModule/Application/Command/Start/StartHandler.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\EngineModule\Application\Command\Start;
+
+
+final class StartHandler
+{
+	public function __invoke(StartCommand $command): void
+	{
+	}
+}
