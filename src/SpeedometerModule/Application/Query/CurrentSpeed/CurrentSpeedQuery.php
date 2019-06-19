2e1c7aa5f74b1ed492a6b5e39447d5e61791b39f
Patryk Woziński <patryk.wozinski@gmail.com>
2019-06-19 13:59:03 +0200

    speedometerid

--- src/SpeedometerModule/Application/Query/CurrentSpeed/CurrentSpeedQuery.php
+++ src/SpeedometerModule/Application/Query/CurrentSpeed/CurrentSpeedQuery.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace App\SpeedometerModule\Application\Query\CurrentSpeed;
 
 
+use App\SpeedometerModule\Domain\SpeedometerId;
+
 interface CurrentSpeedQuery
 {
-	public function byId(int $id): CurrentSpeedReadModel;
+	public function byId(SpeedometerId $id): CurrentSpeedReadModel;
 }
--- src/SpeedometerModule/Infrastructure/ReadModel/InMemorySpeed.php
+++ src/SpeedometerModule/Infrastructure/ReadModel/InMemorySpeed.php
@@ -6,10 +6,11 @@ namespace App\SpeedometerModule\Infrastructure\ReadModel;
 
 use App\SpeedometerModule\Application\Query\CurrentSpeed\CurrentSpeedQuery;
 use App\SpeedometerModule\Application\Query\CurrentSpeed\CurrentSpeedReadModel;
+use App\SpeedometerModule\Domain\SpeedometerId;
 
 final class InMemorySpeed implements CurrentSpeedQuery
 {
-	public function byId(int $id): CurrentSpeedReadModel
+	public function byId(SpeedometerId $id): CurrentSpeedReadModel
 	{
 		return new CurrentSpeedReadModel(60);
 	}
