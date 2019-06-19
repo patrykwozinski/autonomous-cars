3623eb009ca6d4f5b51a09a982917e960afa0bf3
Patryk Woziński <patryk.wozinski@gmail.com>
2019-06-19 13:32:49 +0200

    additionals

--- src/ShiftGearModule/Domain/ShiftGear.php
+++ src/ShiftGearModule/Domain/ShiftGear.php
@@ -6,4 +6,18 @@ namespace App\ShiftGearModule\Domain;
 
 final class ShiftGear
 {
+	private $id;
+
+	public function __construct(ShiftGearId $id)
+	{
+		$this->id = $id;
+	}
+
+	public function higherGear(): void
+	{
+	}
+
+	public function reduceGear(): void
+	{
+	}
 }
