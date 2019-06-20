6d05dd69c93a4dff5c596885eef0f4f2c1a833ce
Patryk Woziński <patryk.wozinski@gmail.com>
2019-06-20 17:34:50 +0200

    reducing gears

--- src/ShiftGearModule/Application/Command/HigherGear/HigherGearCommand.php
+++ src/ShiftGearModule/Application/Command/HigherGear/HigherGearCommand.php
@@ -4,6 +4,19 @@ declare(strict_types=1);
 namespace App\ShiftGearModule\Application\Command\HigherGear;
 
 
+use App\ShiftGearModule\Domain\ShiftGearId;
+
 final class HigherGearCommand
 {
+	private $shiftGearId;
+
+	public function __construct(string $shiftGearId)
+	{
+		$this->shiftGearId = ShiftGearId::fromString($shiftGearId);
+	}
+
+	public function shiftGearId(): ShiftGearId
+	{
+		return $this->shiftGearId;
+	}
 }
